9144527ddc1f8b35bc689fe2b3a52bbb24c0ab8c
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-06-03 23:34:24 -0300

    Creating candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,9 +2,23 @@ pragma solidity >=0.4.21 <0.7.0;
 
 
 contract Election {
-    string public candidate;
-
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+
+    uint256 public candidatesCount;
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
