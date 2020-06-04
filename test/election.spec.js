61980f61a0b4229f3d9c1871044c509356cc2481
Angelo Medeiros <angelomedeiros3@gmail.com>
2020-06-03 23:55:12 -0300

    Creating more tests

--- test/election.spec.js
+++ test/election.spec.js
@@ -6,4 +6,20 @@ contract("Election", (accounts) =>  {
         const countCandidates = await election.candidatesCount()
         assert.equal(countCandidates, 2)
     })
+
+    it ("initializes the candidateOne with the correct values", async () => {
+        const election = await Election.deployed()
+        const candidateOne = await election.candidates(1);
+        assert.equal(candidateOne.id, 1, "contains the correct id")
+        assert.equal(candidateOne.name, "Candidate 1", "contains the correct name")
+        assert.equal(candidateOne.voteCount, 0, "contains the correct votes count")
+    })
+
+    it ("initializes the candidateTwo with the correct values", async () => {
+        const election = await Election.deployed()
+        const candidateOne = await election.candidates(2);
+        assert.equal(candidateOne.id, 2, "contains the correct id")
+        assert.equal(candidateOne.name, "Candidate 2", "contains the correct name")
+        assert.equal(candidateOne.voteCount, 0, "contains the correct votes count")
+    })
 })
